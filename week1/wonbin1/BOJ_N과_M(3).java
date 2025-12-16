4ef2fabdad2d6277bfe29e1d209965f0ea139f29
tray0244 <tray024400@gmail.com>
2025-12-16 17:34:41 +0900

    solve: BOJ_N과_M(3).java

---
+++ week1/wonbin1/BOJ_N과_M(3).java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static boolean[] visited;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        result = new int[M];
+
+        search(0);
+        System.out.println(sb);
+    }
+    public static void search(int depth) {
+        if (depth == M) {
+            for (int num : result) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                result[depth] = i;
+                search(depth + 1);
+            }
+        }
+    }
+
+}
