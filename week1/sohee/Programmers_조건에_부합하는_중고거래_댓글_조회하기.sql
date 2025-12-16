47edde957533f4dc25301d936d1343c10feacd4a
sohee52 <wneh2023@naver.com>
2025-12-16 18:09:12 +0900

    solve: Programmers_조건에_부합하는_중고거래_댓글_조회하기.sql

---
+++ week1/sohee/Programmers_조건에_부합하는_중고거래_댓글_조회하기.sql
@@ -0,0 +1,5 @@
+select b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, date_format(r.CREATED_DATE, "%Y-%m-%d") as CREATED_DATE
+from USED_GOODS_BOARD as b
+join USED_GOODS_REPLY as r on b.BOARD_ID = r.BOARD_ID
+where b.CREATED_DATE like '2022-10-%'
+order by r.CREATED_DATE asc, b.TITLE asc;
