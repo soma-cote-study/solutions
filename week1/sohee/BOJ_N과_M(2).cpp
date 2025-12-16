82bd204b0e193600f51d9e11bef8df24f7fcca68
sohee52 <wneh2023@naver.com>
2025-12-16 20:59:00 +0900

    solve: BOJ_N과_M(2)

---
+++ week1/sohee/BOJ_N과_M(2).cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m, a[10];
+bool visited[10];
+void go(int depth) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (i > a[depth-1] && !visited[i]) {
+            visited[i] = 1;
+            a[depth] = i;
+            go(depth + 1);
+            visited[i] = 0;
+        }
+    }
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+    go(0);
+}
