1b33dde3b0dcb1431411d1571f919d4f3830ba02
tray0244 <tray024400@gmail.com>
2025-12-17 17:36:46 +0900

    solve: BOJ_N과_M(4).java

---
+++ week1/wonbin/BOJ_N과_M(4).java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int N, M;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
+        result = new int[M];
+        search(1, 0);
+        System.out.println(sb);
+    }
+
+    private static void search(int start, int depth){
+        if(depth == M){
+            for(int num : result){
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i <= N; i++){
+            result[depth] = i;
+            search(i, depth + 1);
+        }
+    }
+}
+
