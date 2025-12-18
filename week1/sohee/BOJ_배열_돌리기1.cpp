e7f5f6ca97283bafb1a73af4e6b545d47b84d191
sohee52 <wneh2023@naver.com>
2025-12-18 18:41:20 +0900

    refactor: BOJ_배열_돌리기1.cpp - 필요 없는 level 배열 제거

--- week1/sohee/BOJ_배열_돌리기1.cpp
+++ week1/sohee/BOJ_배열_돌리기1.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m, r, a[304][304], num;
-vector<int> temp, level[154];
+vector<int> temp;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
