f56972ed7d2f90c7c5eb2b1e5daf584d90cc1aa4
sohee52 <wneh2023@naver.com>
2025-12-19 15:48:35 +0900

    refactor: BOJ_N과_M(2).cpp - 탐색 시작값을 매개변수로 전달

--- week1/sohee/BOJ_N과_M(2).cpp
+++ week1/sohee/BOJ_N과_M(2).cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m, a[10];
-void go(int depth) {
+void go(int depth, int start) {
     if (depth == m) {
         for (int i = 0; i < m; i++) {
             cout << a[i] << " ";
@@ -9,16 +9,14 @@ void go(int depth) {
         cout << "\n";
         return;
     }
-    for (int i = 1; i <= n; i++) {
-        if (i > a[depth-1]) {
-            a[depth] = i;
-            go(depth + 1);
-        }
+    for (int i = start; i <= n; i++) {
+        a[depth] = i;
+        go(depth + 1, i + 1);
     }
 }
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> m;
-    go(0);
+    go(0, 1);
 }
