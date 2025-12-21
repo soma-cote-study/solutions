5a2d00c7dfee1f6a0bdee35aa24f87796b48eeec
tray0244 <tray024400@gmail.com>
2025-12-21 23:03:52 +0900

    solve: BOJ_HTML_파싱.java

--- week1/wonbin/Programmers_인기있는_아이스크림.sql
+++
@@ -1,3 +0,0 @@
-select flavor
-from first_half
-order by total_order desc, shipment_id asc;
--- week1/wonbin/Programmers_평균_일일_대여_요금_구하기.sql
+++
@@ -1,3 +0,0 @@
-select round(avg(daily_fee), 0) as average_fee
-from car_rental_company_car
-where car_type = 'suv'
