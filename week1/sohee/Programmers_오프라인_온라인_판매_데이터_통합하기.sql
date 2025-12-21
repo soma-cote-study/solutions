752157625def2bf809598f954719fa58cf0fb4ff
sohee52 <wneh2023@naver.com>
2025-12-22 07:42:54 +0900

    solve: Programmers_오프라인_온라인_판매_데이터_통합하기.sql

---
+++ week1/sohee/Programmers_오프라인_온라인_판매_데이터_통합하기.sql
@@ -0,0 +1,11 @@
+SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date, product_id, user_id, sales_amount
+FROM online_sale
+WHERE DATE_FORMAT(sales_date, '%Y-%m') = '2022-03'
+
+UNION ALL
+
+SELECT DATE_FORMAT(sales_date, '%Y-%m-%d'), product_id, NULL, sales_amount
+FROM offline_sale
+WHERE DATE_FORMAT(sales_date, '%Y-%m') = '2022-03'
+
+ORDER BY sales_date, product_id, user_id;
