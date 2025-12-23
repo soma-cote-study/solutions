b4fe2fd3eeb245d82d0a9f131bfb6a0b2a8c74d6
심유진 <syj11243@naver.com>
2025-12-23 20:25:01 +0900

    solve: Programmers_흉부외과_또는_일반외과_의사_목록_출력하기.sql

--- week1/yujin/Programmers_흉부외과_또는_일반외과_의사_목록_출력하기.sql
+++ week1/yujin/Programmers_흉부외과_또는_일반외과_의사_목록_출력하기.sql
@@ -1,4 +1,5 @@
 select d.dr_name, d.dr_id, d.mcdp_cd, date_format(d.hire_ymd,'%Y-%m-%d') as hire_ymd
 from doctor as d
 where d.mcdp_cd = "CS" or d.mcdp_cd = "GS"
-order by d.hire_ymd desc, d.dr_name asc
+order by d.hire_ymd desc, 
+    d.dr_name asc
