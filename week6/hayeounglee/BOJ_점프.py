461bb73970a446571225597f8cd8b8e2ed76fd66
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2026-01-21 23:16:25 +0900

    solve : BOJ_점프.py

---
+++ week6/hayeounglee/BOJ_점프.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * n for _ in range(n)]
+dp[0][0] = 1
+
+for i in range(n):
+    for j in range(n):
+        jump = board[i][j]
+        if jump == 0:
+            continue
+
+        if i + jump < n:
+            dp[i + jump][j] += dp[i][j]
+
+        if j + jump < n:
+            dp[i][j + jump] += dp[i][j]
+
+print(dp[n - 1][n - 1])
