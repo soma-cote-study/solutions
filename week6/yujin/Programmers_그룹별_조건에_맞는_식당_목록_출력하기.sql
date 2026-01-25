e2762187fb130f3c502d48ddd61d6259ff29cf24
심유진 <syj11243@naver.com>
2026-01-26 01:16:11 +0900

    solve: Programmers_그룹별_조건에_맞는_식당_목록_출력하기.sql

---
+++ week6/yujin/Programmers_그룹별_조건에_맞는_식당_목록_출력하기.sql
@@ -0,0 +1,16 @@
+select p.MEMBER_NAME, r.REVIEW_TEXT, date_format(r.REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
+from REST_REVIEW as r
+join MEMBER_PROFILE as p on p.MEMBER_ID = r.MEMBER_ID
+where r.MEMBER_ID in (select r.MEMBER_ID
+from MEMBER_PROFILE as p
+join REST_REVIEW as r on p.MEMBER_ID = r.MEMBER_ID
+group by r.MEMBER_ID
+having count(*) = (select count(*) as count
+from MEMBER_PROFILE as p
+join REST_REVIEW as r on p.MEMBER_ID = r.MEMBER_ID
+group by r.MEMBER_ID
+order by count desc
+limit 1))
+
+
+order by REVIEW_DATE asc, REVIEW_TEXT asc
