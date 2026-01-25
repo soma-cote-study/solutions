e56e7f6192bafc7c9e75fa43b8cb9a5c41a9393d
심유진 <syj11243@naver.com>
2026-01-26 01:15:54 +0900

    solve: Programmers_5월_식품들의_총매출_조회하기.sql

---
+++ week6/yujin/Programmers_5월_식품들의_총매출_조회하기.sql
@@ -0,0 +1,6 @@
+select p.product_id, p.product_name, sum(o.amount)*p.price as total_sales
+from FOOD_PRODUCT as p
+join FOOD_ORDER as o on p.product_id = o.product_id
+where date_format(o.PRODUCE_DATE, "%Y-%m") = '2022-05'
+group by p.product_id
+order by total_sales desc, p.product_id asc
